759ecf1362db5f6e825551c45e0d780b3ba16689
iGlaciem <>
2023-03-20 21:30:12 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-let paragraph = document.querySelector(".option-one-screen")
-paragraph.style.display = "none";
+// Get references to the HTML elements
+const optionOneButton = document.querySelector('.option-one');
+const optionTwoButton = document.querySelector('.option-two');
+const optionOneScreen = document.querySelector('.option-one-screen');
+const optionTwoScreen = document.querySelector('.option-two-screen');
+const optionOneEnd = document.querySelector('.option-one-end');
+const optionTwoEnd = document.querySelector('.option-two-end');
+
+// Add event listeners to the buttons to show the appropriate screen when clicked
+optionOneButton.addEventListener('click', function() {
+optionOneScreen.style.display = 'block';
+});
+
+optionTwoButton.addEventListener('click', function() {
+optionTwoScreen.style.display = 'block';
+});
+
+// Add event listeners to the screens to show the appropriate end screen when finished
+optionOneScreen.addEventListener('click', function() {
+optionOneScreen.style.display = 'none';
+optionOneEnd.style.display = 'block';
+});
+
+optionTwoScreen.addEventListener('click', function() {
+optionTwoScreen.style.display = 'none';
+optionTwoEnd.style.display = 'block';
+});
